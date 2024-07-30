0d04ff0371764b46532d4d1b363f763949249053
FranzOle <115760743+FranzOle@users.noreply.github.com>
2024-07-30 13:46:26 +0700

    Video 13
    Activity Life Cycle

---
+++ MessageDialogue/app/src/main/java/com/example/messagedialogue/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.messagedialogue;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        System.out.println("on create");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        System.out.println("on start");
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        System.out.println("on resume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("on pause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        System.out.println("on stop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.out.println("on destroy");
+    }
+
+}
