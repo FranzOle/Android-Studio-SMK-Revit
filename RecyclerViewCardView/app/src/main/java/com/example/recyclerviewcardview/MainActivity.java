ca5b589ba75ccb09459f1e4ec87a5503c4972105
FranzOle <115760743+FranzOle@users.noreply.github.com>
2024-07-31 13:33:51 +0700

    Video 20-22
    RecyclerView layout-models

---
+++ RecyclerViewCardView/app/src/main/java/com/example/recyclerviewcardview/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.recyclerviewcardview;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    SiswaAdapter adapter;
+    List<Siswa> SiswaList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        load();
+        isiData();
+    }
+
+    public void load() {
+        recyclerView = findViewById(R.id.rcvSiswa);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    public void isiData() {
+        SiswaList = new ArrayList<Siswa>();
+        SiswaList.add(new Siswa("Joni", "Ngawi"));
+        SiswaList.add(new Siswa("Beni", "Ngawi"));
+        SiswaList.add(new Siswa("Didit", "Ngawi"));
+        SiswaList.add(new Siswa("Losman", "Ngawi"));
+        adapter = new SiswaAdapter(this,SiswaList);
+        recyclerView.setAdapter(adapter);
+    }
+}
